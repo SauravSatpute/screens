57c6fc5dda06322a5f25f0f3c90ee158d00bb003
Saurav Satpute <66636762+SauravSatpute@users.noreply.github.com>
2025-01-26 20:52:59 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,1 @@
-export const USER_AVATAR = 'https://occ-0-3241-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229'
 
-export const IMG_CDN_URL = 'https://image.tmdb.org/t/p/w500'
-
-
-export const movieDBToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZWVjYjUzZTg5NTU3NzUwNzA4MjgyYTMwMjA3NzY4MSIsIm5iZiI6MTcyOTY2Njc1MC41NjE4OTksInN1YiI6IjY1YTU2YWVmODI4OWEwMDBjNmFkYWQ4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.83gAbHzfkJ25m25gTOigbvgiPZC7kxl0SqXahdp15mM'
